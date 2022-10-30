bd36c0ad84646654e32457b2605744f77401722f
EMOS <sourbar2001@gmail.com>
2022-10-30 13:28:58 +0100

    version 1

--- PizzamamamDjango/pizzamamam/menu/views.py
+++ PizzamamamDjango/pizzamamam/menu/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Pizza
+# Create your views here.
+def index(request):
+    Pizzas = Pizza.objects.all().order_by("prix")
+    """Pizzas_names =[Pizza.nom + " : "+ str(Pizza.prix) + "£" for Pizza in Pizzas]
+    Pizzas_names_str= ", ".join(Pizzas_names)"""
+    return render(request, 'menu/index.html', {"Pizzas": Pizzas})
